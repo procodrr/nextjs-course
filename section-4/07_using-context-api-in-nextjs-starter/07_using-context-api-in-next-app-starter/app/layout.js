f9e5346644610c436f3ea27864de8efadb4a6963
ProCodrr <procodrr@gmail.com>
2025-03-15 18:06:49 +0530

    fix section 4 chapters code

--- section-4/07_using-context-api-in-nextjs-starter/07_using-context-api-in-next-app-starter/app/layout.js
+++ section-4/07_using-context-api-in-nextjs-starter/07_using-context-api-in-next-app-starter/app/layout.js
@@ -1,8 +1,9 @@
+import Link from "next/link";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body>{children}</body>
     </html>
   );
